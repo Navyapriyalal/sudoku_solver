e7d1b32fd0f177a2b8a9a3a92f310b7cd4469d65
Navyapriyalal <navyapriyalal2004@gmail.com>
2024-07-26 15:17:12 +0530

    commited

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -32,3 +32,45 @@ class Box:
                 if j==0:
                     count+=1
         return count
+
+def check_row(boxes,box_num,row,ele):
+    row_ind = (row-1)%3
+    if box_num == 1 or box_num == 2 or box_num == 3:
+        for i in range(0,3):
+            if ele in boxes[i].box[row_ind]:
+                return False
+    elif box_num == 4 or box_num == 5 or box_num == 6:
+        for i in range(3,6):
+            if ele in boxes[i].box[row_ind]:
+                return False
+    elif box_num == 7 or box_num == 8 or box_num == 9:
+        for i in range(6,9):
+            if ele in boxes[i].box[row_ind]:
+                return False
+    else:
+        print('Not Valid Box number')
+        return
+    return True
+
+def check_col(boxes,box_num,col,ele):
+    col_ind = (col-1)%3
+    if box_num == 1 or box_num == 4 or box_num == 7:
+        for i in range(0,7,3):
+            for j in range(3):
+                if boxes[i].box[j][col_ind] == ele:
+                    return False
+    elif box_num == 2 or box_num == 5 or box_num == 8:
+        for i in range(1,8,3):
+            for j in range(3):
+                if boxes[i].box[j][col_ind] == ele:
+                    return False
+    elif box_num == 3 or box_num == 6 or box_num == 9:
+        for i in range(2,9,3):
+            for j in range(3):
+                if boxes[i].box[j][col_ind] == ele:
+                    return False
+    else:
+        print('Not Valid Box number')
+        return
+    return True
+
